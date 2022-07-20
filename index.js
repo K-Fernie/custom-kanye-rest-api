5ddc98535cc9d0868afba133ef7472862813d6fc
Katy Fernelius <kkin001@gmail.com>
2022-07-20 17:39:06 -0500

    added port correction

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 //middleware
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
